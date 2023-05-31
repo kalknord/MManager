12b03ba921b69c4c3e1b28c342063fd5603bdc99
kalknord <133772407+kalknord@users.noreply.github.com>
2023-05-31 13:23:18 +0200

    test

--- src/MManager/Einstellungverhältnis/Manager.java
+++ src/MManager/Einstellungverhältnis/Manager.java
@@ -11,17 +11,6 @@ class Manager extends Mitarbeiter {
         this.projekt = projekt;
     }
 
-    public String mitarbeiterInfoAnzeigen() {
-        StringBuilder output = new StringBuilder();
-        output.append("Manager:\n");
-        output.append("Vorname: ").append(getVorname()).append("\n");
-        output.append("Nachname: ").append(getNachname()).append("\n");
-        output.append("Personalnummer: ").append(getPersonalnummer()).append("\n");
-        output.append("Abteilung: ").append(getAbteilung()).append("\n");
-        output.append("Projekt: ").append(getProjekt()).append("\n\n");
-        return output.toString();
-    }
-
     public String getProjekt() {
         return projekt;
     }
